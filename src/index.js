f72c811098202b07aad7220e5e532b7427a53e51
HauptmanDev <MainVados@gmail.com>
2021-11-02 13:04:00 +0300

    Corrected type of the store.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
+import App from './App';
 import {Provider} from "react-redux";
 import {store} from "./store/store.ts";
-import AppTs from "./AppTs";
 
 ReactDOM.render(
   <React.StrictMode>
       <Provider store={store}>
-          {/*<App/>*/}
-          <AppTs/>
+          <App/>
       </Provider>
   </React.StrictMode>,
   document.getElementById('root')
