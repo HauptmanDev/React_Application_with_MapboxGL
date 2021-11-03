4129ba7456dac802bedceb17c86f8e2017895df2
HauptmanDev <MainVados@gmail.com>
2021-11-03 12:15:48 +0300

    Corrected index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import {Provider} from "react-redux";
 import {store} from "./store/store.ts";
+import AppTs from "./AppTs";
 
 ReactDOM.render(
   <React.StrictMode>
       <Provider store={store}>
-          <App/>
+          {/*<App/>*/}
+          <AppTs/>
       </Provider>
   </React.StrictMode>,
   document.getElementById('root')
